7e8a5885ef79d96c2ad0d677beaecbbba470f8f9
SincerelyFaust <hedesmarin@gmail.com>
2023-02-06 19:30:56 +0100

    sorry next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import "../styles/globals.css";
-import type AppProps from "next/app";
+import { AppProps } from "next/app";
 
 function FSKSatellite({ Component, pageProps }: AppProps) {
   return (
